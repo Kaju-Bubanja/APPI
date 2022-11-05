96ea790cccbb1adbc4ed257b9133c218db98eab8
Kaju Bubanja <kaju.bubanja@cesag.com>
2022-11-05 22:28:07 +0100

    Reduce line length

--- src/appi/oop/solutions.py
+++ src/appi/oop/solutions.py
@@ -136,10 +136,12 @@ print(car)
 # Color: White, Vehicle name: Audi Q5, Speed: 240, Mileage: 18
 
 
-# 6: Create a Bus child class that inherits from the Vehicle class. The default fare charge of any vehicle is seating capacity * 100.
+# 6: Create a Bus child class that inherits from the Vehicle class. The default fare charge of any vehicle is
+# seating capacity * 100.
 # If Vehicle is Bus instance, we need to add an extra 10% on full fare as a maintenance charge.
 # So total fare for bus instance will become the final amount = total fare + 10% of the total fare.
-# Note: The bus seating capacity is 50. so the final fare amount should be 5500. You need to override the fare() method of a Vehicle class in Bus class.
+# Note: The bus seating capacity is 50. so the final fare amount should be 5500. You need to override the fare()
+# method of a Vehicle class in Bus class.
 # Given
 class Vehicle:
     def __init__(self, name, mileage, capacity):

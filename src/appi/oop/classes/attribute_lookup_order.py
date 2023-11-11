d37e48466bfc84f1b7401425683409dc74f6a7ed
Kaju Bubanja <bubanja.kaju@gmail.com>
2023-11-11 16:29:25 +0100

    feat: add section about previously used syntax

--- src/appi/oop/classes/attribute_lookup_order.py
+++ src/appi/oop/classes/attribute_lookup_order.py
@@ -10,3 +10,14 @@ print(w1.region)
 # prints "east"
 print(Warehouse.region)
 # prints "west"
+
+
+# Reframing previously used syntax (constructors) and object methods
+x = []
+x = list()
+x.append(5)
+print(x)
+
+y = dict()
+y = {"2": "abc"}
+print(y.items())

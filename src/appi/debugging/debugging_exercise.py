a9758c934c851f02734237ccebc54450ac6e763e
Kaju Bubanja <bubanja.kaju@gmail.com>
2023-11-11 16:32:12 +0100

    feat: add example of how to catch Exception and use operator methods

--- src/appi/debugging/debugging_exercise.py
+++ src/appi/debugging/debugging_exercise.py
@@ -9,6 +9,12 @@ from log_handling import setup_logger, close_log_handlers
 # Catch at least 3 specific exceptions and log a sensible message
 # Allow the user to use the last result by typing "last" instead of a number as an input
 # You can use following dictionary to retrieve operators and use the operator like so operator.add(x, y)
+# example how to catch an Exception
+try:
+    int("abc")
+except ValueError:
+    print("Can't convert to number")
+# print(2+2) == print(operator.add(2, 2))
 ops = {
     "+": operator.add,
     "-": operator.sub,
